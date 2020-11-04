6d1fb622007b5e5e6654e87232bc453801ea4485
junshen1969 <51145478+junshen1969@users.noreply.github.com>
2020-11-05 09:28:23 +1100

    Update list_certs.sh

--- list_certs.sh
+++ list_certs.sh
@@ -7,7 +7,7 @@ DOMAIN=$5
 NUM_ARGS=$#
 
 INFILE=$DP_HOST-$DOMAIN-list_certs.cmd
-OUTFILE=$DP_HOTS-$DOMAIN-list_certs.txt
+OUTFILE=$DP_HOST-$DOMAIN-list_certs.txt
 
 display_usage() {
   echo -e "\nUsage:\n $0 dp_host_or_ip dp_ssh_port dp_admin dp_pass dp_domain\n"
